2847e4252b82fb313128e26ce0c6391b82db64da
Chris Whiting <mpro34@gmail.com>
2022-01-23 10:39:04 -0800

    Add lib kafka and create a consumer

--- CryptoMatch/CryptoMatch.cpp
+++ CryptoMatch/CryptoMatch.cpp
@@ -3,13 +3,45 @@
 
 #include <iostream>
 #include "engine/order_book.hpp"
+#include <librdkafka/rdkafka.h>
+#include <signal.h>
 
 int main()
 {
     std::cout << "CryptoMatch Engine Online!\n";
 
+    rd_kafka_topic_t* rkt;
+    char* topic = NULL;
+    rd_kafka_conf_t* conf;
+    rd_kafka_topic_conf_t* topic_conf;
+    char errstr[512];
+    char tmp[16];
+
+    /* Create default Kafka Configuration */
+    conf = rd_kafka_conf_new();
+
+    /* Quick termination setup */
+    snprintf(tmp, sizeof(tmp), "%i", SIGINT);
+    rd_kafka_conf_set(conf, "internal.termination.signal", tmp, NULL, 0);
+
+    /* Topic configuration */
+    topic_conf = rd_kafka_topic_conf_new();
+
     // TODO: Create a consumer and listen for new orders - Kafka
-    // https://docs.confluent.io/clients-librdkafka/current/overview.html
+
+    rd_kafka_conf_set(conf, "enable.partition.eof", "true", NULL, 0);
+
+    /* Create kafka consumer handle*/
+    rd_kafka_t* consumer_handle = rd_kafka_new(RD_KAFKA_CONSUMER, conf, errstr, sizeof(errstr));
+    if (consumer_handle == nullptr)
+    {
+        fprintf(stderr, "%% Failed to create a new consumer: %s\n", errstr);
+        exit(1);
+    }
+
+    /* Create topic */
+    rkt = rd_kafka_topic_new(consumer_handle, topic, topic_conf);
+    topic_conf = NULL; // Transfer ownership to topic.
 
     // TODO: Create a producer for trade messages
 
